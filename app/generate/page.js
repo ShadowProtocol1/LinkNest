58d5c42b3bb2c761315b543a4f4596fe96175f63
ShadowProtocol1 <uietcseyayaativerma2125@kuk.ac.in>
2024-12-03 08:02:32 +0000

    changes

--- app/generate/page.js
+++ app/generate/page.js
@@ -72,14 +72,14 @@ const Generate = () => {
   return (
     <div className='bg-[#E9C0E9] min-h-screen grid grid-cols-2'>
      
-      <div className="col1 flex justify-center items-center flex-col text-gray-900">
-        <div className='flex flex-col gap-5 mt-24'>
+      <div className="col1 flex justify-center items-center flex-col text-gray-900 w-full ml-20">
+        <div className='flex flex-col gap-5 mt-20'>
           <h1 className='font-bold text-4xl'>Create your Bittree</h1>
           <div className="item">
 
             <h2 className='font-semibold text-2xl'>Step 1: Claim your Handle</h2>
             <div className='mx-4'>
-              <input value={handle || ""} onChange={e=>{sethandle(e.target.value)}} className='px-4 py-2 my-2 focus:outline-pink-500 rounded-full'
+              <input value={handle || ""} onChange={e=>{sethandle(e.target.value)}} className='px-4 mx-2 py-2 mt-2 focus:outline-pink-500 rounded-full'
                 type="text" placeholder='Choose a Handle' />
             </div>
           </div>
@@ -88,24 +88,24 @@ const Generate = () => {
             {links && links.map((item, index)=>{
               return <div key={index} className='mx-4'>
               <input value={item.linktext || ""} onChange={e=>{handleChange(index, item.link, e.target.value)}} className='px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-full' type="text" placeholder='Enter link text' />
-              <input value={item.link || ""} onChange={e=>{handleChange(index, e.target.value, item.linktext)}} className='px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-full'
+              <input value={item.link || ""} onChange={e=>{handleChange(index, e.target.value, item.linktext)}} className='px-4 py-2 mx-2 focus:outline-pink-500 rounded-full'
                 type="text" placeholder='Enter link' />
             </div>
             })}
-              <button onClick={()=> addLink()} className='p-5 py-2 mx-2 bg-slate-900 text-white font-bold rounded-3xl'>+ Add Link</button>
+            <button onClick={()=> addLink()} className='p-5 py-2 mx-6 bg-slate-900 text-white font-bold rounded-3xl'>+ Add Link</button>
           </div>
 
           <div className="item">
             <h2 className='font-semibold text-2xl'>Step 3: Add Picture and Description</h2>
             <div className='mx-4 flex flex-col'>
               <input value={pic || ""} onChange={e=>{setpic(e.target.value)}} className='px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-full' type="text" placeholder='Enter link to your Picture' />
-              <input value={desc || ""} onChange={e=>{setdesc(e.target.value)}} className='px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-full' type="text" placeholder='Enter description' />
-              <button disabled={pic == "" || handle=="" || links[0].linktext == ""} onClick={()=>{submitLinks()}} className='disabled:bg-slate-500 p-5 py-2 mx-2 w-fit my-5 bg-slate-900 text-white font-bold rounded-3xl'>Create your BitTree</button>
+              <input value={desc || ""} onChange={e=>{setdesc(e.target.value)}} className='px-4 py-2 mx-2  focus:outline-pink-500 rounded-full' type="text" placeholder='Enter description' />
+              <button disabled={pic == "" || handle=="" || links[0].linktext == ""} onClick={()=>{submitLinks()}} className='disabled:bg-slate-500 p-5 py-2 mx-2 w-fit my-4 bg-slate-900 text-white font-bold rounded-3xl'>Create your BitTree</button>
             </div>
           </div>
         </div>
       </div>
-      <div className="col2 w-full h-screen bg-[#E9C0E9]">
+      <div className="col2 w-fit max-h-screen bg-[#E9C0E9] ml-32">
         <img className='h-full object-contain' src="/generate.png" alt="Generate your links" />
         <ToastContainer />
       </div>
