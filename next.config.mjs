7cbbd5d9f57647c32c644a9597adc5aed2a90710
ShadowProtocol1 <uietcseyayaativerma2125@kuk.ac.in>
2024-12-03 14:22:00 +0000

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+
+module.exports = {
+    experimental: {
+      missingSuspenseWithCSRBailout: false,
+    },
+  }
+
 export default nextConfig;
